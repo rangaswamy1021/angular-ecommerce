7ca3b9eb45f0bbb7954112313c84d78ae8ab4630
Ranga Swamy <33976154+rangaswamy1021@users.noreply.github.com>
2023-12-20 17:31:44 +0530

    App route config file changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'products',
+  },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('@features/products/products.routes').then(
+        (m) => m.PRODUCTS_ROUTES
+      ),
+  },
+];
